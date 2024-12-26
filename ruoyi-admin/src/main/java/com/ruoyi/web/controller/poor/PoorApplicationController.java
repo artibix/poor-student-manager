3dad310402eda5be74900a17f981a813ee974824
artibix <artibix@proton.me>
2024-12-26 11:32:52 +0800

    build(admin): 移除贫困模块相关代码
    - 删除了 PoorApplicationController 中的 PoorApplication 相关代码
    - 移除了 pom.xml 中的 art-poor 依赖- 删除了 applicationMenu.sql、financialMenu.sql 和 quartz.sql 文件

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/poor/PoorApplicationController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/poor/PoorApplicationController.java
@@ -2,6 +2,9 @@ package com.ruoyi.web.controller.poor;
 
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ruoyi.poor.domain.PoorApplication;
+import com.ruoyi.poor.service.IPoorApplicationService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +19,6 @@ import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.enums.BusinessType;
-import com.ruoyi.poor.domain.PoorApplication;
-import com.ruoyi.poor.service.IPoorApplicationService;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.common.core.page.TableDataInfo;
 
